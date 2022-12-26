3b0c5a8fac1e6ae09a5f335389ff7af4f8d856f3
Drajke <drajke@gmail.com>
2022-12-26 18:49:12 +0300

    Added additional Selenide lesson part 1

---
+++ src/test/java/PageHaveTextSecond.java
@@ -0,0 +1,16 @@
+import com.codeborne.selenide.Selenide;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
+import org.junit.jupiter.api.Test;
+
+public class PageHaveTextSecond {
+    //На главной странице GitHub выберите меню Solutions -> Enterprize с помощью команды hover для Solutions.
+    // Убедитесь что загрузилась нужная страница (например что заголовок - "Build like the best."
+    @Test
+    void GitHoverTest() {
+        Selenide.open("https://github.com/");
+        Selenide.$(byText("Solutions")).hover();
+        Selenide.$(byText("Enterprise")).click();
+        Selenide.$("div.col-9-max h1").shouldHave(text("Build like the best"));
+    }
+}
